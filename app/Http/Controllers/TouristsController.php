58564c8ce2208b4876cc6011b75f0e02834144e6
Naoki0722 <mattu.nao722@gmail.com>
2020-12-20 23:53:31 +0900

    202012202353時点

--- app/Http/Controllers/TouristsController.php
+++ app/Http/Controllers/TouristsController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tourist;
+use App\Models\Comment;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 
 class TouristsController extends Controller
 {
@@ -37,9 +40,24 @@ class TouristsController extends Controller
         $item->image_path = $request->image_path;
         $item->pref_id = $request->pref_id;
         $item->save();
+
+        // 一緒にコメントも送る
+        $comment = new Comment;
+        $now = Carbon::now();
+        $comment->tourist_id = $item->id;
+        $comment->name = $request->name;
+        $comment->title = $request->title;
+        $comment->review = $request->review;
+        $comment->comment = $request->comment;
+        $comment->image_path = $request->image_path;
+        $comment->created_at = $now;
+        $comment->updated_at = $now;
+        $comment->save();
+
         return response() -> json([
-            'message' => 'tourist_data created successfully',
-            'data' => $item
+            'message' => 'tourist_data and comment created successfully',
+            'data' => $item,
+            'commentData' => $comment
         ],200);
     }
 
