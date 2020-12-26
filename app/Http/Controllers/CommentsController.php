ee408342b3055c6dccc9aff9b2625d4a8a6eeea7
Naoki0722 <mattu.nao722@gmail.com>
2020-12-26 17:49:00 +0900

    20201226

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Comment;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class CommentsController extends Controller
 {
@@ -30,6 +32,25 @@ class CommentsController extends Controller
      */
     public function store(Request $request)
     {
+        //画像処理記載
+        $filename = $request->img;
+        $filename = preg_replace('/^data:image.*base64,/','', $filename);
+        $filename = str_replace(' ','+', $filename);
+        $image = base64_decode($filename);
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mine_type = finfo_buffer($finfo, $image);
+        $extenstions = [
+            'image/gif' => 'gif',
+            'image/jpeg' => 'jpeg',
+            'image/png' => 'png'
+        ];
+        //保存ファイル名はランダム表記
+        $random_str = Str::random(10);
+        $filename = $random_str . '.' . $extenstions[$mine_type];
+        $store_dir = 'comments';
+        $storefile = sprintf('%s/%s', $store_dir, $filename);
+        Storage::disk('s3')->put($storefile, $image);
+        $image_path = Storage::disk('s3')->url($storefile);
         $comment = new Comment;
         $now = Carbon::now();
         $comment->tourist_id = $request->tourist_id;
@@ -37,7 +58,7 @@ class CommentsController extends Controller
         $comment->title = $request->title;
         $comment->review = $request->review;
         $comment->comment = $request->comment;
-        $comment->image_path = $request->image_path;
+        $comment->image_path = $image_path;
         $comment->created_at = $now;
         $comment->updated_at = $now;
         $comment->save();
--- app/Http/Controllers/TouristsController.php
+++ app/Http/Controllers/TouristsController.php
@@ -7,6 +7,8 @@ use App\Models\Comment;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 
 class TouristsController extends Controller
@@ -17,11 +19,18 @@ class TouristsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        $items = Tourist::all();
+    {        
+        $tourists = DB::table('tourists')
+            ->select(DB::raw('pref_id,tourists.id, place_name , description, round(avg(review),1) as review'))
+            ->join('comments', 'tourists.id', '=', 'comments.tourist_id')
+            ->groupBy('tourist_id')
+            ->orderBy('review', 'desc')
+            ->limit(3)
+            ->get();
+        clock($tourists);
         return response()->json([
             'message' => 'tourist_datas got successfully',
-            'data' => $items
+            'data' => $tourists,
         ], 200);
     }
 
@@ -33,15 +42,53 @@ class TouristsController extends Controller
      */
     public function store(Request $request)
     {
+
         //観光地データをインサートする
+        //まずは、画像処理記載
+        $file_name = $request->tourist_img;
+        $file_name = preg_replace('/^data:image.*base64,/', '', $file_name);
+        $file_name = str_replace(' ', '+', $file_name);
+        $image_info = base64_decode($file_name);
+        $file_info = finfo_open(FILEINFO_MIME_TYPE);
+        $file_mine_type = finfo_buffer($file_info, $image_info);
+        $extenstions = [
+            'image/gif' => 'gif',
+            'image/jpeg' => 'jpeg',
+            'image/png' => 'png'
+        ];
+        //保存ファイル名はランダム表記
+        $random_text = Str::random(10);
+        $img_name = $random_text . '.' . $extenstions[$file_mine_type];
+        $store_fld = 'comments';
+        $store_file = sprintf('%s/%s', $store_fld, $img_name);
+        Storage::disk('s3')->put($store_file, $image_info);
+        $place_image_path = Storage::disk('s3')->url($store_file);
+
+        //観光地情報
         $item = new Tourist;
         $item->place_name = $request->place_name;
         $item->description = $request->description;
-        $item->place_image_path = $request->place_image_path;
+        $item->place_image_path = $place_image_path;
         $item->pref_id = $request->pref_id;
         $item->save();
 
         // 一緒にコメントも送る
+        //まずは、画像処理記載
+        $filename = $request->img;
+        $filename = preg_replace('/^data:image.*base64,/', '', $filename);
+        $filename = str_replace(' ', '+', $filename);
+        $image = base64_decode($filename);
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mine_type = finfo_buffer($finfo, $image);
+        //保存ファイル名はランダム表記
+        $random_str = Str::random(10);
+        $filename = $random_str . '.' . $extenstions[$mine_type];
+        $store_dir = 'comments';
+        $storefile = sprintf('%s/%s', $store_dir, $filename);
+        Storage::disk('s3')->put($storefile, $image);
+        $image_path = Storage::disk('s3')->url($storefile);
+
+        //テキスト
         $comment = new Comment;
         $now = Carbon::now();
         $comment->tourist_id = $item->id;
@@ -49,7 +96,7 @@ class TouristsController extends Controller
         $comment->title = $request->title;
         $comment->review = $request->review;
         $comment->comment = $request->comment;
-        $comment->image_path = $request->image_path;
+        $comment->image_path = $image_path;
         $comment->created_at = $now;
         $comment->updated_at = $now;
         $comment->save();
